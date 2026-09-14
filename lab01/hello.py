7696b51867ed43e8a8759a2dcef7f04ecada6f34
Jimbo01111 <c00309477@setu.ie>
2026-09-14 14:46:17 +0000

    Complete Lab 1

---
+++ lab01/hello.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+course = input("What is your course? ")
+favArea = input("What is your favourite area of cybersecurity? ")
+
+print("Hello, " + name + "!")
+print("You are studying " + course + "!")
+print("You are interestested in " + favArea + "!")
+
---
+++ lab01/student.py
@@ -0,0 +1,7 @@
+fullName = input(" What is your full name? ")
+studentNo = input("What is your student number? ")
+yearStudy = input("What is your current year of study? ")
+
+print("Name: " + fullName)
+print("Student Number: " + studentNo)
+print("Year: " + yearStudy)
